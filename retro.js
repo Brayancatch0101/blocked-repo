72c33c17bc33f578f5990ef20602d819be47de79
Brayan Cathorn <154638871+Brayancatch0101@users.noreply.github.com>
2023-12-23 00:31:18 +0200

    Create retro.js

---
+++ retro.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/@next/ApiReq/*',
+    '!**/@next/utils/*',
+    '!**/@next/hooks/*',
+    '!**/@next/store/slices/auth/*',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/pages/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**'
+  ],
+
+
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/\\.next/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+        https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx|svg)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$']
+}
+
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    /* Handle CSS imports (with CSS modules)
+
+        https://jestjs.io/docs/webpack#mocking-css-modules */
+    '\\.svg$': '<rootDir>/__mocks__/svg.js',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
